f85b26d58050a8ffcbd2a25dce332e695f14281b
Nishit <nishitranjan41@gmail.com>
2022-07-05 20:14:19 +0530

    [factors.java]this is my vowelorconsonant.java

---
+++ vowelconsonant.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class vowelconsonant {
+
+        public static void main(String[] args)
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("input a character");
+            char ch=sc.next().charAt(0);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+                System.out.println(ch + " is vowel");
+            else
+                System.out.println(ch+"is consonant");
+
+        }
+    }
